34dc81d6a12a7da0e025366925bd29ce03e60bc6
DimaKatout <dimakatout97@gmail.com>
2020-02-26 13:16:40 +0200

    this is the updated javascript code

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+"use strict";
+
+alert('welcomee');
+var hello =prompt('please enter your name ');
+alert('hello' + hello);
+
+
+var cat=prompt('do you think i have a phobia from cats ?');
+//cat.toLocaleLowerCase()
+//console.log(cat);
+if (cat=='N'.toLocaleLowerCase()||cat=='NO'.toLocaleLowerCase())
+{alert('you got it right ')}
+else
+{alert('you got it wrong ')}
+
+var favCol=prompt('do you think red is my favorite color ');
+if(favCol=='Y'||favCol=='YES')
+{alert('you got it correct')}
+else
+{alert('wrong awnser')}
+
+var age=prompt('do you think i\'m 23? ');
+if (age=='Y'||age=='YES')
+{alert('you got it right');}
+else
+{alert('please try for the next time ')}
+ 
+var smart=prompt('do you think im smart ')
+if (smart=='Y'||smart=='YES')
+{alert('you got it correct');}
+else 
+{alert('noo wrong')}
+
+alert('thank you for visiting ');
