7dde69e93ac7781d184987f90c44d37b9761463b
DimaKatout <dimakatout97@gmail.com>
2020-02-26 15:27:17 +0200

    updated java with case

--- js/main.js
+++ js/main.js
@@ -2,34 +2,55 @@
 "use strict";
 
 alert('welcomee');
-var hello =prompt('please enter your name ');
+var hello = prompt('please enter your name ');
 alert('hello' + hello);
 
 
-var cat=prompt('do you think i have a phobia from cats ?');
+var cat = prompt('do you think i have a phobia from cats ?');
 //cat.toLocaleLowerCase()
 //console.log(cat);
-if (cat=='N'.toLocaleLowerCase()||cat=='NO'.toLocaleLowerCase())
-{alert('you got it right ')}
-else
-{alert('you got it wrong ')}
+if (cat == 'N'.toLocaleLowerCase() || cat == 'NO'.toLocaleLowerCase()) { alert('you got it right ') }
+else { alert('you got it wrong ') }
 
-var favCol=prompt('do you think red is my favorite color ');
-if(favCol=='Y'||favCol=='YES')
-{alert('you got it correct')}
-else
-{alert('wrong awnser')}
+var favCol = prompt('do you think red is my favorite color ');
+if (favCol == 'Y' || favCol == 'YES') { alert('you got it correct') }
+else { alert('wrong awnser') }
 
-var age=prompt('do you think i\'m 23? ');
-if (age=='Y'||age=='YES')
-{alert('you got it right');}
-else
-{alert('please try for the next time ')}
- 
-var smart=prompt('do you think im smart ')
-if (smart=='Y'||smart=='YES')
-{alert('you got it correct');}
-else 
-{alert('noo wrong')}
+var age = prompt('do you think i\'m 23? ');
+if (age == 'Y' || age == 'YES') { alert('you got it right'); }
+else { alert('please try for the next time ') }
+
+var smart = prompt('do you think im smart ')
+if (smart == 'Y' || smart == 'YES') { alert('you got it correct'); }
+else { alert('noo wrong') }
+
+
+var food = prompt('do i love food? ');
+cat.toLowerCase();
+switch (cat) {//hoon ba7ot bel case small letters la2no hala2 kol shi 7ayd5ol 3aliha 7aykon t7awal la small
+
+    case 'y':
+    case 'yes':
+        alert('yes you got it correct');
+        break;
+
+    case 'n':
+    case 'no':
+        alert('you got it wrong');
+        break;
+
+
 
 alert('thank you for visiting ');
+
+
+
+
+        
+
+
+
+
+
+
+}
